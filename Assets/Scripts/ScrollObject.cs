34e0f22cbd6e69f0272611ade44b052b5c3a9bc2
Imnyeong <lmh4873@gmail.com>
2024-05-25 18:49:19 +0900

    feat: aseest 버전 업데이트, character animation 추가

---
+++ Assets/Scripts/ScrollObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float moveValue = -0.01f;
+    public float startPosition;
+    public float endPosition;
+
+    public bool down = false;
+
+    private float coroutineDelay = 0.01f;
+    void Start()
+    {
+        StartCoroutine(ScrollCoroutine());
+    }
+
+    private IEnumerator ScrollCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(coroutineDelay);
+
+        if (!down)
+        {
+            if (transform.position.x <= endPosition)
+            {
+                ScrollEnd();
+            }
+            else
+            {
+                transform.Translate(speed * -0.01f, 0, 0);
+            }
+        }
+        StartCoroutine(ScrollCoroutine());
+    }
+    void ScrollEnd()
+    {
+        transform.Translate(-1 * (endPosition - startPosition), 0 , 0);
+    }
+}
